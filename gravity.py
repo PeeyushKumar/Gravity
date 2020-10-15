4a0fab8cd54fd141fce23af0e8532483cfab5f46
Inder Bhushan Jha <47220493+inderbhushanjha@users.noreply.github.com>
2020-10-15 22:48:44 +0530

    Update gravity.py
    gravity simulation with shapes as object.

--- gravity.py
+++ gravity.py
@@ -1,101 +1,64 @@
-import pygame
-import time
+import turtle
 import random
-import math
 
 
-width = 600
-height = 400
-radius = 50
-amount = 2
+wn=turtle.Screen()
+wn.bgcolor("black")
+wn.title("gravity")
+wn.tracer(0)
+
+
 balls = []
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+color=["red","yellow","orange","pink","purple","white","violet"]
+shape=["circle","triangle","square"]
+
+for bal in range(50):
+	balls.append(turtle.Turtle())
+
+for ball in balls:
+	ball.shape(random.choice(shape))
+	x=random.randint(-290,290)
+	y=random.randint(-200,400)
+	ball.goto(x,y)
+	ball.color(random.choice(color))
+	ball.penup()
+	ball.speed(0)
+	ball.dy=-1
+	ball.dx=random.randint(-3,3)
+	ball.dz=random.randint(-5,-5)
+
+gravity=0.1
+loop=True
 
 
 
-class object:
-	def __init__(self, color, rad, xspeed, yspeed):
-		self.color = color
-		self.xcor = 0
-		self.ycor = 0
-		self.rad = rad
-		self.xspeed = xspeed
-		self.yspeed = yspeed
 
-	def move(self, xcor, ycor):
-		self.xcor = xcor
-		self.ycor = ycor
-
-
-def position(x,y):
-	x = random.randint(radius, width-radius)
-	y = random.randint(radius, height-radius)
+while loop:
+	wn.update()
 	for ball in balls:
-		if math.sqrt((ball.xcor - x)**2 + (ball.ycor - y)**2) <= radius*2:
-			x = random.randint(radius, width-radius)
-			y = random.randint(radius, height-radius)
+		ball.sety(ball.ycor() + ball.dy)
+		ball.dy-=gravity
+		ball.setx(ball.xcor() + ball.dx)
+		ball.rt(ball.dz)
 
-	return (x,y)
+		if ball.xcor() > 330:
+			ball.dx*=-1
 
+		if ball.xcor() < -330:
+			ball.dx*=-1
 
-def convert_y(y):
-	y -= height
-	y *= -1
-	return y
-
-
-for index in range(amount):
-	x = y = 0
-	cor = position(x,y)
-	speedx = random.random()*5
-	balls.append(object(random.choice(colors), radius, speedx, 0))
-	balls[index].move(cor[0],cor[1])
+		if ball.ycor() < -270 :
+			ball.sety(-270)
+			ball.dy*=-1
+			ball.dy-=1
 
 
-pygame.init()
-win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Gravity")
-
-running = True
-while running:
-	win.fill((255,255,255))
-	for ball in balls:
-		for Ball in balls:
-			if ball is not Ball:
-
-				if math.sqrt((ball.xcor - Ball.xcor)**2 + (ball.ycor - Ball.ycor)**2) <= radius*2:
-
-					temp = ball.xspeed
-					ball.xspeed = Ball.xspeed
-					Ball.xspeed = temp
-
-					temp = ball.yspeed
-					ball.yspeed = Ball.yspeed
-					Ball.yspeed = temp
 
 
-		ball.yspeed -= 0.1
-
-		if ball.ycor - radius <= 0:
-
-			ball.yspeed *= -1
-
-			if ball.yspeed**2 < 2:
-				ball.yspeed = 0
-				
-
-		if ball.xcor + radius >= width:
-			ball.xspeed *= -1
-		elif ball.xcor - radius <= 0:
-			ball.xspeed *= -1
 
 
-		ball.ycor += ball.yspeed
-		ball.xcor += ball.xspeed
 
 
-		pygame.draw.circle(win, ball.color, (int(ball.xcor), int(convert_y(ball.ycor))), ball.rad)
 
 
-	pygame.display.update()
-	time.sleep(0.01)
+wn.mainloop()
